cefe29372636b9e22043ff0667f990008615502e
Ardian Eka Candra <ardianoption@gmail.com>
2026-07-02 10:01:09 +0700

    refactor: remove unused detectTextDirection (replaced by native dir=auto)

--- packages/core/src/lib/detect-direction.ts
+++
@@ -1,5 +0,0 @@
-export function detectTextDirection(text: string): "ltr" | "rtl" {
-  const rtlChars =
-    /[\u0590-\u05FF\u0600-\u06FF\u0700-\u074F\u0750-\u077F\u08A0-\u08FF\uFB1D-\uFDFF\uFE70-\uFEFF]/;
-  return rtlChars.test(text) ? "rtl" : "ltr";
-}
